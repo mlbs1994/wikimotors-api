e8801cb2657f95401fab73836e06ca6574adbd84
mlbs1994 <matheuslevibarouh@gmail.com>
2023-09-19 10:05:27 -0300

    Simplicando DTOs de resumo: AutomovelResumoDTO e FabricanteResumoDTO

--- wikimotors-api/src/main/java/com/wikimotors/api/model/automovel/dtos/AutomovelResumoDTO.java
+++ wikimotors-api/src/main/java/com/wikimotors/api/model/automovel/dtos/AutomovelResumoDTO.java
@@ -1,18 +1,17 @@
 package com.wikimotors.api.model.automovel.dtos;
 
 import com.wikimotors.api.model.automovel.entities.Automovel;
-import com.wikimotors.api.model.fabricante.dtos.FabricanteResumoDTO;
 
 public record AutomovelResumoDTO(
-		FabricanteResumoDTO fabricante,
 		String modelo,
+		String fabricante,
 		int anoFabricacao,
 		String categoria) {
 	
 	public AutomovelResumoDTO(Automovel automovel) {
 		this(
-				new FabricanteResumoDTO(automovel.getFabricante()),
 				automovel.getModelo(),
+				automovel.getFabricante().getNome(),
 				automovel.getAnoFabricacao(),
 				automovel.getCategoria().getDescricao());
 	}
--- wikimotors-api/src/main/java/com/wikimotors/api/model/fabricante/dtos/FabricanteResumoDTO.java
+++ wikimotors-api/src/main/java/com/wikimotors/api/model/fabricante/dtos/FabricanteResumoDTO.java
@@ -2,9 +2,9 @@ package com.wikimotors.api.model.fabricante.dtos;
 
 import com.wikimotors.api.model.fabricante.entities.Fabricante;
 
-public record FabricanteResumoDTO(String nome, String logo_img) {
+public record FabricanteResumoDTO(String nome, String pais) {
 	
 	public FabricanteResumoDTO(Fabricante fabricante) {
-		this(fabricante.getNome(), fabricante.getLogo_img());
+		this(fabricante.getNome(), fabricante.getPais());
 	}
 }
